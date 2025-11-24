df949df4b8e3f3ca4d604eea958d8677a14065e8
Tempest-Zero <81361769+Tempest-Zero@users.noreply.github.com>
2025-11-24 16:07:20 +0500

    class + list implementation in main
    I passed a class object to the post function so fast api automatically validates that the data coming from the server is akin to our objects attributes i.e the data we expect it to give.
    Also put dictionaries in a list and then fetched that list from get function also passed into to_post (post function)

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from fastapi import FastAPI
 from fastapi.params import Body 
 app = FastAPI()
-
+class Post(BaseModel):
+    name: str
+    goal: str
+    published: bool = True
+    rating: Optional[int] = None
 @app.get("/root")
 async def root():
     return {"message": "Hello World"}
+    
+my_posts = [ { "title": "totle of post 1", "content": "content of post 1", "id": 1 }, {"title":"favourite foods","content":"I like pizza", "id": 2}]   #list
 
 @app.get("/posts")
 async def get_posts():
-    return {"data: This is your posts"}
+    return {"data" : my_posts}
 
-@app.post("/posting")
-async def to_post(payLoad : dict = Body(...)):
-    print(payLoad)
-    return {
-        "new_post" : f"name: {payLoad['name']} goal :{payLoad['goal']}"
-    }
+@app.post("/posts");
+async def to_post(post: Post):
+    print(post.rating)
+    return {"data": my_posts }
 
 
 
