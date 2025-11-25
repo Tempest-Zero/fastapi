8d03564f6b4375fb6b935f96fd965c34ccbeb446
Tempest-Zero <81361769+Tempest-Zero@users.noreply.github.com>
2025-11-25 16:19:04 +0500

    storing new post details in memory (array/list) python
    converted json to dict using model_dump function and stored the new post into our list, used a random number generator to generate a unique id for our posts and append it to the dict for unique identification.

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body 
+from pydantic import BaseModel
+from random import randrange
 app = FastAPI()
+
+
 class Post(BaseModel):
     name: str
     goal: str
     published: bool = True
     rating: Optional[int] = None
+ 
+my_posts = [ { "title": "title of post 1", "content": "content of post 1", "id": 1 }, {"title":"favourite foods","content":"I like pizza", "id": 2}]   #list
 @app.get("/root")
 async def root():
     return {"message": "Hello World"}
-    
-my_posts = [ { "title": "totle of post 1", "content": "content of post 1", "id": 1 }, {"title":"favourite foods","content":"I like pizza", "id": 2}]   #list
 
 @app.get("/posts")
 async def get_posts():
-    return {"data" : my_posts}
+    return {"data": my_posts}
 
-@app.post("/posts");
-async def to_post(post: Post):
-    print(post.rating)
-    return {"data": my_posts }
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.model_dump(); #we are not using databases rn so we will have to generate id from software
+                                   #otherwise the database handles ids
+    post_dict['id'] = randrange(0,1000000) #selecting from a very large range
+                                           #ensures there's a very small probability
+                                           #of getting the same number twice
+    my_posts.append(post_dict)
 
-
+    
 
 
 
