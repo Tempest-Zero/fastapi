222316f9dfe0b266f93ca546e55b6e1dc0b52033
Tempest-Zero <81361769+Tempest-Zero@users.noreply.github.com>
2025-11-26 17:24:39 +0500

    delete function added
    to delete posts

--- main.py
+++ main.py
@@ -42,8 +42,21 @@ def get_post(id:int, response: Response):
     #  response.status_code = status.HTTP_404_NOT_FOUND
     #  return {'message': f"post with id: {id} was not found"}
     return {"post_detail": post}
-    
+
+@app.delete("/posts/{id}", status_code = status.HTTP_204_NO_CONTENT)
+def delete_post(id:int):
+    #deleting post
+    # find the index in the array that has required ID
+    # my_posts.pop(index)
+    index = find_index_post(id);
+    if index == None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist ")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
      
+
 
 
 
